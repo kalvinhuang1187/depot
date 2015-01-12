9a5d6f26e3efcf71e0c1aeba3e05d810b00f540a
Kalvin Huang <kalvin_huang@yahoo.com>
2015-01-12 08:59:41 -0800

    replace multiple items for a single product in a cart with single item and count

--- test/helpers/carts_helper_test.rb
+++ test/helpers/carts_helper_test.rb
@@ -1,4 +1,11 @@
 require 'test_helper'
 
 class CartsHelperTest < ActionView::TestCase
+  current_item = line_items.find_by_product_id(product_id)
+  if current_item
+    current_item.quantity += 1
+  else
+    current_item = line_items.build(product_id: product_id)
+  end
+  current_item
 end
