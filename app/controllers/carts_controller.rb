217a6207f58d49735c324262b2fbdf2636abeceb
Kalvin Huang <kalvin_huang@yahoo.com>
2015-01-20 17:17:34 -0800

    hide an empty cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -70,8 +70,7 @@ class CartsController < ApplicationController
     session[:cart_id] = nil
 
     respond_to do |format|
-      format.html { redirect_to carts_url, 
-        notice: 'Cart was currently empty.' }
+      format.html { redirect_to store_url }
       format.json { head :no_content }
     end
   end
